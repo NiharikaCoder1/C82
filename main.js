d8aac6aea129c693f63b52fcbea8e5d07bcd7444
NiharikaCoder1 <123148466+NiharikaCoder1@users.noreply.github.com>
2023-01-20 19:15:02 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var canvas= document.getElementById('c1');
+ctx= canvas.getContext('2d');
+
+
+var MouseEvent= "";
+canvas.addEventListener('mousedown', my_mousedown);
+function my_mousedown(e){
+MouseEvent='mouseDown';
+}
+
+canvas.addEventListener('mouseup', my_mouseup);
+function my_mouseup(e){
+MouseEvent='mouseUp';
+}
+
+canvas.addEventListener('mouseleave', my_mouseleave);
+function my_mouseleave(e){
+MouseEvent='mouseLeave';
+}
+
+canvas.addEventListener('mousemove', my_mousemove);
+function my_mousemove(e){
+ CPX=e.clientX-canvas.offsetLeft;
+ CPY=e.clientY-canvas.offsetTop;
+    if(MouseEvent=='mouseDown'){
+        ctx.beginPath();
+        ctx.strokeStyle='red';
+        ctx.lineWidth=2;
+        ctx.moveTo(lpx, lpy);
+        ctx.lineTo(CPX, CPY)
+        ctx.stroke();
+    }
+    lpx=CPX;
+    lpy=CPY;
+}
